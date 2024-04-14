79010ce39d63e17055c44e9a94709ebad0ee30bf
Srikanth K <kolleparasrikanth@gmail.com>
2024-04-14 12:16:41 +0530

    initial configuration of index.js

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import path from "path";
 import multer from "multer";
+import bodyParser from "body-parser";
+import csv from "csv-parser";
+import expressLayouts from "express-ejs-layouts";
 
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(expressLayouts);//setting Layout
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('./public')); 
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
